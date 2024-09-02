a33bee4f41ccc35f77a6ee2f5380ce39bf7862f6
SurajToraskar <toraskarsurajsuryakant@gmail.com>
2024-09-02 23:02:47 +0530

    appwrite

--- BlogApp/src/appwrite/auth.js
+++ BlogApp/src/appwrite/auth.js
@@ -1,12 +1,61 @@
 import conf from "../conf/conf.js";
 import { Client, Account, ID } from "appwrite";
 
-const client = new Client()
-  .setEndpoint(conf.appwriteUrl)
-  .setProject(conf.appwriteProjectId);
+export class AuthService {
+  client = new Client();
+  account;
 
-const account = new Account(client);
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+    this.account = new Account(this.client);
+  }
 
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      if (userAccount) {
+        return this.login({ email, password });
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
 
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailPasswordSession(email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
 
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      throw error;
+    }
+    return null;
+  }
 
+  async logout() {
+    try {
+      await this.account.deleteSessionsf();
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+const authService = new AuthService();
+
+export default authService;
